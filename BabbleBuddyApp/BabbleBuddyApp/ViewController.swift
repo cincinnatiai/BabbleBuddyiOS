0898d564824e60d5862ef7d409f1038fb68c1325
Dionicio <dioniciocruzvelazquez@gmail.com>
2025-03-25 10:55:57 -0400

    Initial commit

---
+++ BabbleBuddyApp/BabbleBuddyApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BabbleBuddyApp
+//
+//  Created by Dionicio Cruz Velázquez on 3/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ NetworkingKit/NetworkingKit/NetworkingKit.h
@@ -0,0 +1,18 @@
+//
+//  NetworkingKit.h
+//  NetworkingKit
+//
+//  Created by Dionicio Cruz Velázquez on 3/25/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkingKit.
+FOUNDATION_EXPORT double NetworkingKitVersionNumber;
+
+//! Project version string for NetworkingKit.
+FOUNDATION_EXPORT const unsigned char NetworkingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingKit/PublicHeader.h>
+
+
